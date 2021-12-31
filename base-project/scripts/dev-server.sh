1b21507c72774238596d99ac3c6ca0eb374ead12
André Rodrigues <andre.rodrigues98@outlook.com>
2021-12-30 23:25:16 -0300

    improving scripts package.json

---
+++ base-project/scripts/dev-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+PORT=[port of app] DEBUG=[level of debug. default value is 'app:*'] USER_DB=[username of postgres database] HOST_DB=[host of database. default value is database_pg] DATABASE_NAME=[database name] PASSWORD_DB=[password for postgres database] PORT_DB=[database port] SECRET=[secret to generate jwt token] ts-node-dev --transpile-only --ignore-watch node_modules src/server.ts
--- base-project/src/server.ts
+++ base-project/src/server.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 
 import appRoutes from './modules/app/routes';
+import appConfig from './config/app';
 import handleErrors from './modules/app/middlewares/handle-errors';
 import DebugLogProvider from './modules/app/providers/implementations/DebugLogProvider';
 import messages from './modules/app/intl/messages/en-US';
@@ -19,6 +20,6 @@ app.use('/', appRoutes);
 
 app.use(handleErrors)
 
-app.listen(3333, () => {
+app.listen(appConfig.settings.port, () => {
   debugLogProvider.essentialsLog({ message: messages.logs.INITIAL_LOG });
 });
